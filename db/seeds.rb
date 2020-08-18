a957728fc25b8df15f55dbefd56980b25bc64864
Diego Oliveira <diegomaka@gmail.com>
2020-08-18 16:43:50 -0300

    Wiki articles created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(title: Faker::App.name,
+                        content: Faker::TvShows::RickAndMorty.quote)
+  article.save!
+end
+# puts 'Finished!'
+
